bf95ce1189e9441641e0a52f7bfff9e2368bf97e
BrownEdgar <nikoghosjanedgar@gmail.com>
2023-10-20 12:19:54 +0400

    first test push

--- src/users/Edgar/App.jsx
+++ src/users/Edgar/App.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function App() {
+  const [count, setCount] = useState(0)
   return (
-    <div>App</div>
+    <div>
+      <h1>{count}</h1>
+      <button onClick={() => setCount(count + 1)}>add count</button>
+    </div>
   )
 }
