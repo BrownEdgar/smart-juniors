5af2800ead4166b2ad5b7a3a46c573576b93cb0a
BrownEdgar <nikoghosjanedgar@gmail.com>
2023-10-20 12:28:19 +0400

    added git rools in readme file

---
+++ README.md
@@ -0,0 +1,7 @@
+# git option
+
+1. git status
+2. git add .
+3. git commit -m "your message here"
+4. git pull
+5. git push
--- src/users/Edgar/App.jsx
+++ src/users/Edgar/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 
 export default function App() {
   const [count, setCount] = useState(0)
