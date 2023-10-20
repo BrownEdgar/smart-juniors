922db2243557165e9b911506f74ac50b64f708f8
Hayk_Developer <hayktemuryan0@gmail.com>
2023-10-20 23:47:10 +0400

    homework 1 complated

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
+// import App from './users/Hayk/homework_1/App.jsx' // Hayk
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
