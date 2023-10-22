6695b8a55839ffd96752df4b2a410788968d4b48
G-RM-N <std4rm@gmail.com>
2023-10-22 18:35:22 +0400

    homework 1: useState info on screen

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
 // import App from './users/Hayk/homework_1/App.jsx' // Hayk
 
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
