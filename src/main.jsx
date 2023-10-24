b8f9ca99573ffa40687e8fa6f0094b5d2e3b2714
Tsovinar <tsovinarstepanyan@gmail.com>
2023-10-24 11:52:05 +0300

    git

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 // import App from './users/Hayk/homework_1/App.jsx' // Hayk
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './users/Edgar/classNames/App';
+import App from './App'
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
