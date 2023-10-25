bae0054b237eef39114c976f9b24c565325108bf
BrownEdgar <nikoghosjanedgar@gmail.com>
2023-10-25 12:34:51 +0400

    added props children example

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import App from './users/Edgar/classNames/App';
+import App from './users/Edgar/props_children/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
--- src/users/Hayk/homework_2/components/Users/Users.jsx
+++ src/users/Hayk/homework_2/components/Users/Users.jsx
@@ -5,55 +5,55 @@ import "./Users.scss"
 import classNames from 'classnames';
 
 export default function Users({ users, setProfile }) {
-    const [selected, setSelected] = useState(null)
+  const [selected, setSelected] = useState(null)
 
-    return (
-        <ul className='Users'>
-            {
-                users.map(user => {
-                    return (
-                        <li key={user.id} className={classNames('Users-child', {
-                            [`Users-child_active`]: user.id === selected ? true : false
-                        })} onClick={() => {
-                            setProfile(user)
-                            setSelected(user.id)
-                        }}>
-                            <User user={user} />
-                        </li>
-                    )
-                }
-                )
-            }
-        </ul>
-    )
+  return (
+    <ul className='Users'>
+      {
+        users.map(user => {
+          return (
+            <li key={user.id} className={classNames('Users-child', {
+              [`Users-child_active`]: user.id === selected
+            })} onClick={() => {
+              setProfile(user)
+              setSelected(user.id)
+            }}>
+              <User user={user} />
+            </li>
+          )
+        }
+        )
+      }
+    </ul>
+  )
 }
 
 Users.propTypes = {
-    users: PropTypes.arrayOf(
-        PropTypes.exact({
-            id: PropTypes.number.isRequired,
-            name: PropTypes.string.isRequired,
-            username: PropTypes.string.isRequired,
-            email: PropTypes.string.isRequired,
-            address: PropTypes.exact({
-                street: PropTypes.string.isRequired,
-                suite: PropTypes.string.isRequired,
-                city: PropTypes.string.isRequired,
-                zipcode: PropTypes.string.isRequired,
-                geo: PropTypes.exact({
-                    lat: PropTypes.string.isRequired,
-                    lng: PropTypes.string.isRequired,
-                })
-            }),
-            phone: PropTypes.string.isRequired,
-            website: PropTypes.string.isRequired,
-            company: PropTypes.exact({
-                name: PropTypes.string.isRequired,
-                catchPhrase: PropTypes.string.isRequired,
-                bs: PropTypes.string.isRequired
-            })
+  users: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+      username: PropTypes.string.isRequired,
+      email: PropTypes.string.isRequired,
+      address: PropTypes.exact({
+        street: PropTypes.string.isRequired,
+        suite: PropTypes.string.isRequired,
+        city: PropTypes.string.isRequired,
+        zipcode: PropTypes.string.isRequired,
+        geo: PropTypes.exact({
+          lat: PropTypes.string.isRequired,
+          lng: PropTypes.string.isRequired,
         })
-    )
+      }),
+      phone: PropTypes.string.isRequired,
+      website: PropTypes.string.isRequired,
+      company: PropTypes.exact({
+        name: PropTypes.string.isRequired,
+        catchPhrase: PropTypes.string.isRequired,
+        bs: PropTypes.string.isRequired
+      })
+    })
+  )
 }
 
 
