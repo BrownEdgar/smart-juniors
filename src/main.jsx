99dbfb53c885a8f8b81b6944d2d94198d0d8f08c
G-RM-N <std4rm@gmail.com>
2023-10-26 02:01:31 +0400

    recreated classNames lesson

--- src/main.jsx
+++ src/main.jsx
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom/client';
 import App from './users/Edgar/props_children/App';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
