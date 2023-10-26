81d98c98265aaf782db56c44bbd6e34c11ec59fc
Mikayel Sahakyan <mikayel92@gmail.com>
2023-10-26 12:15:15 +0400

    Classwork - PropsChildren

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@
 // import App from './users/Hrach/propTypes/App'
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from '../src/users/Mikayel/Classwork/props_children/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
