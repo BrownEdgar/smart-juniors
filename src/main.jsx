a9ad49d76f168e1ed0853bf4162a6621874c96af
Ach7772 <hrach14x@mail.ru>
2023-10-27 01:13:32 +0400

    Modal homework

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,9 @@
-<<<<<<< HEAD
-// import App from './users/Hayk/homework_1/App.jsx' // Hayk
-// import App from './users/Hrach/propTypes/App'
+// import App from './users/Hrach/Modal/App'
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App
-
-
-
+import App from './App'
 import './index.css';
-=======
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
->>>>>>> 6bbce12e56db79a3b53298e50ef9cf5c95dd8376
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
--- src/users/Vahe/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function App() {
-  return (
-    <div>
-      <h1>Armen homeworks</h1>
-    </div>
-  )
-}
