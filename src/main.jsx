56fc6ad8d172da71d5d3d487c3c88c0204b7806d
Ach7772 <hrach14x@mail.ru>
2023-10-31 03:38:49 +0400

    hmwrk Form

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './users/Edgar/FORM/App'
+import App from './users/Hrach/hmwrk_Formik/App'
 
 import './index.css';
 
