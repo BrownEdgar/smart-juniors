4ec80e305a4f1af561da6da7adc7f3710bbda8d8
Mikayel Sahakyan <mikayel92@gmail.com>
2023-11-03 18:01:29 +0400

    Classwork - useCustom Hook

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-=======
 import React from 'react';
->>>>>>> bfc40a01ddf4bdb01d681a51fbfc122f24e33994
 import ReactDOM from 'react-dom/client';
 import App from './App'
 import './index.css';
