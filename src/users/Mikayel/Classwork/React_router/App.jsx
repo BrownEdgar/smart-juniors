d857b30864995fe0e1e373c60eb3f69acb638d47
Mikayel Sahakyan <mikayel92@gmail.com>
2023-11-08 17:50:43 +0400

    Classwork - React_router_Posts

--- src/users/Mikayel/Classwork/React_router/App.jsx
+++ src/users/Mikayel/Classwork/React_router/App.jsx
@@ -1,9 +1,10 @@
 import { useState } from 'react'
-import { Home, About, Blog, ErrorPage } from './pages'
+import { Home, About, Blog, ErrorPage, Posts, Post } from './pages'
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import ROUTES from './routes/routes'
 import Layouts from './components/Layouts/Layouts'
-
+import { postLoader } from './pages/Posts'
+import './App.scss'
 
 
 export default function App() {
@@ -15,6 +16,8 @@ export default function App() {
         <Route index element={<Home />} />
         <Route path={ROUTES.ABOUT} element={<About />} />
         <Route path={ROUTES.BLOG} element={<Blog value={value} />} />
+        <Route path={ROUTES.POSTS} element={<Posts />} loader={postLoader} />
+        <Route path={ROUTES.POST} element={<Post />} />
         {/* <Route path='*' element={<Navigate to={ROUTES.HOME} />} /> */}
         <Route path='*' element={<ErrorPage />} />
       </Route>
