3fa08d680d0712bf823631b3c9523a900a9702d0
Tsovinar <tsovinarstepanyan@gmail.com>
2023-11-09 21:39:19 +0300

    router

--- src/users/Tsovinar/Register/App.jsx
+++ src/users/Tsovinar/Register/App.jsx
@@ -7,9 +7,11 @@ import Layouts from './Components/Layaout/Layouts'
 import "./App.scss"
 import { useState, useEffect } from 'react'
 import ROUTES from './Routes/routes'
+import Errorpage from './Pages/Errorpage'
+import User from './Pages/User'
 
 export default function App() {
-    const[users, setUsers] = useState([44])
+    const[users, setUsers] = useState([])
     
 
     const router = createBrowserRouter(
@@ -17,7 +19,8 @@ export default function App() {
         <Route path= {ROUTES.REGISTER} element={<Layouts />}>
             <Route index element={<Register setUsers= {setUsers} users={users} />} />
             <Route path={ROUTES.USERS} element={<Users users={users} /> } />
-            <Route path='*' element='/register' />
+            <Route path={ROUTES.USER} element={<User users={users}/> } />
+            <Route path='*' element={<Errorpage />} />
         </Route>
         )
     )
--- src/users/Tsovinar/Register/Pages/Users.jsx
+++ src/users/Tsovinar/Register/Pages/Users.jsx
@@ -1,15 +1,35 @@
 import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
 
 export default function Users({users}) {
+  
   return (
     <div className='users'>
-      <h2>Users list </h2>
-      {
-        users.length!=0 ? 
-        users.map((elm, ind) => <li key={ind} className='userlist'> firstname: {elm.FirstName}, lastname:{elm.LastName} , age: {elm.age}</li> )
-        :
-        null
-      }   
+     <table>
+      <tbody>
+        <tr>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Age</th>
+        </tr>
+        {
+          users.map((user, index) =>{
+            return(
+              <tr key={index}>
+                <td>
+                  <Link to={`${user.FirstName}`}>
+                    {user.FirstName}
+                  </Link>
+                </td>
+                <td>{user.LastName}</td>
+                <td>{user.age}</td>
+              </tr>
+            )
+          })
+        }
+      </tbody>
+     </table>
+            
     </div>
   )
 }
