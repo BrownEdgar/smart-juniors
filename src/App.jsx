f280980ae425988d88ac068ac596125e7da7b07f
BrownEdgar <nikoghosjanedgar@gmail.com>
2023-11-13 11:53:00 +0400

    fix title bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <div>
-      <h1>Hello react in git</h1>
+      <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi quae quaerat hic vero eveniet fuga obcaecati sed quisquam voluptatibus! Assumenda suscipit aliquid perspiciatis provident molestiae voluptatem, velit expedita facilis. Qui quae commodi quas dolore architecto harum, dolor officiis eaque nisi.</h1>
     </div>
   )
 }
