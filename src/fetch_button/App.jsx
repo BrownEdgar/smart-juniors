5050318554ecb7c7aee0c33b71d724bda5b2c853
BrownEdgar <nikoghosjanedgar@gmail.com>
2023-11-17 11:00:11 +0400

    changed code variant

--- src/fetch_button/App.jsx
+++ src/fetch_button/App.jsx
@@ -1,37 +1,36 @@
 import { useState } from "react"
 import "./App.scss"
 import FetchButton from "./components/FetchButton/FetchButton"
+import classNames from 'classnames'
+
+const arr = ["A", "B", "C"]
 
 export default function App() {
-  const [char, setChar] = useState("A")
+  const [char, setChar] = useState(arr[0])
 
   const handleClick = (e) => {
-    setChar(e.target.textContent)
-    e.target.classList.add("active")
-    let next = e.target?.nextElementSibling;
-    let prev = e.target?.previousElementSibling;
-
-    while("BUTTON" === next?.nodeName || prev?.nodeName) {
-      if(next) {
-        next.classList.remove("active")
-        next = next?.nextElementSibling
-      }
-      if(prev) {
-        prev.classList.remove("active")
-        prev = prev?.previousElementSibling
-      }
-    } 
+    setChar(e)
   }
 
   return (
     <div className='App'>
       <div className="App-header">
         <h1>{char}</h1>
-        <button onClick={(e) => handleClick(e)}>A</button>
-        <button onClick={(e) => handleClick(e)}>B</button>
-        <button onClick={(e) => handleClick(e)}>C</button>
+        {arr.map(elem => {
+          return (
+            <button
+              key={elem}
+              className={classNames({
+                active: elem === char
+              })}
+              onClick={() => handleClick(elem)}>
+              {elem}
+            </button>
+          )
+        })}
+
       </div>
-      <FetchButton/>
+      <FetchButton />
     </div>
   )
 }
