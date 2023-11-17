2657bea6bb911fd6024699153c4c90918d863b70
BrownEdgar <nikoghosjanedgar@gmail.com>
2023-11-17 11:11:40 +0400

    change hw

--- src/HW/App.jsx
+++ src/HW/App.jsx
@@ -1,48 +1,35 @@
-import  {useState} from 'react'
+import { useState } from 'react'
 import './App.scss'
 import classNames from 'classnames'
 
+const buttonsArray = ['One', "Two", "Three"]
+
 export default function App() {
-    const [text, setText] = useState(null)
-    const [isActive, setisActive] = useState(false)
-    const [buttontwoisActive, setbuttontwoisActive] = useState(null)
-    const [buttonthreeisActive, setbuttonthreeisActive] = useState(null)
+  const [text, setText] = useState(buttonsArray[0])
 
 
-    
-    const firstButtonClick = (e) => {
-        setText(e.target.name)
-        setisActive(!isActive)
-        setbuttontwoisActive(false)
-        setbuttonthreeisActive(false)
-    
-    }
-
-    const secondButtonClick = (e)=>{
-        setText(e.target.name)
-        setisActive(false)
-        setbuttontwoisActive(true)
-        setbuttonthreeisActive(false)    
-    }
-
-    const thirdButtonClick = (e)=>{
-        setText(e.target.name)
-        setisActive(false)
-        setbuttontwoisActive(false)
-        setbuttonthreeisActive(true)   
-    }
-
-   
+  const toggleActive = (elem) => setText(elem)
   return (
     <>
-    
-    <div className='buttons'>
-        <h1>{text}</h1>
-        <button className={classNames(null,{'green': isActive})} onClick={firstButtonClick} type='submit' name='A'>A</button>
-        <button className={classNames(null,{'green': buttontwoisActive})}  onClick={secondButtonClick} type='submit' name='B'>B</button>
-        <button className={classNames(null,{'green': buttonthreeisActive})} onClick={thirdButtonClick} type='submit' name='C'>C</button>
 
-    </div>
+      <div className='buttons'>
+        <h1>{text}</h1>
+        {
+          buttonsArray.map(elem => {
+            return (
+              <button
+                key={elem}
+                className={classNames({
+                  [`btn-${elem}`]: true
+                })}
+                onClick={() => toggleActive(elem)}>
+                {elem}
+              </button>
+            )
+          })
+        }
+
+      </div>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client';
-import App from './debounce/App'
+import App from './HW/App'
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
