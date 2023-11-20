10d04173bfeb22039eba5667184c17d1c531b284
BrownEdgar <nikoghosjanedgar@gmail.com>
2023-11-20 11:11:03 +0400

    fix main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import ReactDOM from 'react-dom/client';
 
 import App from './useContext/App'
 
-import App from './HW/App'
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
