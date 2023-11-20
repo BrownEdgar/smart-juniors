6bf29eb5dc1e56b1c1ad2b7c4f0b5306eac8d19c
BrownEdgar <nikoghosjanedgar@gmail.com>
2023-11-20 11:19:43 +0400

    fix spaces

--- src/users/Karen/useContext/App.jsx
+++ src/users/Karen/useContext/App.jsx
@@ -1,53 +1,49 @@
-import { useEffect, useState} from   'react'
-import A from             './components/A'
-import axios from         'axios'
+import { useEffect, useState } from 'react'
+import A from './components/A'
+import axios from 'axios'
 import { MyContext } from './contexts/dataContext'
-import                    './App.scss'
+import './App.scss'
 
 
 
 export default function App() {
 
-const [data, setData] = useState([])
-// const [data1, setData1] = useState([])
+  const [data, setData] = useState([])
+  // const [data1, setData1] = useState([])
 
 
-// useEffect(()=>{
-//   axios('http://localhost:3000/users')
-//   .then(res=>setData(res.data))
-// },[])
-
-// useEffect(()=>{
-//   axios('http://localhost:3000/users')
-//   .then(res=>setData(res.data))
-// },[])
+  // useEffect(()=>{
+  //   axios('http://localhost:3000/users')
+  //   .then(res=>setData(res.data))
+  // },[])
 
 
 
-localStorage.setItem('data',JSON.stringify(data))
-console.log(data);
 
-const getData=()=>{ 
-  axios('http://localhost:3000/users')
-  .then(res=>setData(res.data))
-  JSON.parse(localStorage.getItem('data1')||'[]')
-}
+  localStorage.setItem('data', JSON.stringify(data))
+  console.log(data);
+
+  const getData = () => {
+    axios('http://localhost:3000/users')
+      .then(res => setData(res.data))
+
+  }
 
 
-const deleteData=()=>{
-  setData(data.toSpliced(data,1))
-     }
+  const deleteData = () => {
+    setData(data.toSpliced(data, 1))
+  }
 
   return (
     <div>
-         <MyContext.Provider value={{
-            data,
-            // data1,
-            getData,
-            deleteData
-            }}>
-           <A/>
-        </MyContext.Provider> 
+      <MyContext.Provider value={{
+        data,
+        // data1,
+        getData,
+        deleteData
+      }}>
+        <A />
+      </MyContext.Provider>
     </div>
   )
 }
