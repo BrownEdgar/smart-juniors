c3b5f4e8edb3dbdbd9306efdb04e08d26143442f
G-RM-N <std4rm@gmail.com>
2023-11-22 15:24:35 +0400

    added Provide from react-dedux for redux in main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { addCount } from './features/count/countSlice';
+
 export default function App() {
-  return (
-    <div>
-      <h1>Hello react in git</h1>
-    </div>
-  )
+	const state = useSelector((state) => state);
+	const dispatch = useDispatch();
+
+	const handleClick = () => {
+		dispatch(addCount());
+	};
+
+	return (
+		<div>
+			<h1>Hello react in git {state.count}</h1>
+			<button onClick={handleClick}>Add count</button>
+		</div>
+	);
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,11 @@
 import ReactDOM from 'react-dom/client';
-import App from './Hooks/useContext/App';
+import App from './App';
+import { Provider } from 'react-redux';
+import store from './app/store';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<Provider store={store}>
+		<App />
+	</Provider>
+);
