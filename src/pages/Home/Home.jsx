9ae390cef1db4a306b90bd05de81e7c063e96aeb
G-RM-N <std4rm@gmail.com>
2023-11-29 11:30:17 +0400

    homepage changes

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import './Home.scss';
 
 export default function Home() {
 	return (
-		<section className="Home">
+		<section className="Home centerAlign">
 			<h6 className="Home-title small">React</h6>
 			<h1 className="Home-title large">Redux</h1>
 		</section>
