efc8f926f0640e655dcc7d2b91ef931f44c75209
Hayk_Developer <hayktemuryan0@gmail.com>
2023-12-03 23:12:37 +0400

    react-slick slider

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react'
-import App from './redux/redux_4/App';
-import store from './redux/redux_4/app/store';
-import {persistor} from "./redux/redux_4/app/store";
+import App from './react_slick/App';
+import store from './react_slick/app/store';
+// import { PersistGate } from 'redux-persist/integration/react'
+// import {persistor} from "./redux/redux_4/app/store";
 
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
+    {/* <PersistGate loading={null} persistor={persistor}> */}
       <App />
-    </PersistGate>
+    {/* </PersistGate> */}
   </Provider>
 );
