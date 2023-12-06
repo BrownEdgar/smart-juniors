ece87e839c3b9ce08aa2ba1e6616df61af0c219a
BrownEdgar <nikoghosjanedgar@gmail.com>
2023-12-06 10:59:39 +0400

    added pagination example

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client';
-import App from './users/Edgar/Slider/MainSlider';
+import App from './users/Edgar/Pagination/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
